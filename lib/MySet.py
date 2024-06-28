8c5d315701d26790956d2bdd10d435f94e80f5ef
Wambui <nderituwambui93@gmail.com>
2024-06-29 00:24:42 +0300

    updating labs

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,26 @@
 class MySet:
 
-    pass
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
+        
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+    def clear(self):
+        self.dictionary = {}
+        return self
+    def size(self):
+        return len(self.dictionary)
+    def __str__(self):
+        set_list = []
+        for key, value in self.dictionary.items():
+            set_list.append(str(key))
+        return f'MySet: {{{",".join(set_list)}}}'
